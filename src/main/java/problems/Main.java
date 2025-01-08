9efbfb48455a2c15959c543004d9c56260a357e9
katsiarynahaspadaryk <gaspodarik98@gmail.com>
2025-01-08 15:08:52 +0200

    merged gitignore conflict

--- src/main/java/problems/Main.java
+++ src/main/java/problems/Main.java
@@ -3,7 +3,6 @@ package problems;
 public class Main {
     public static void main(String[] args) {
 
-//        System.out.println(new InsertInterval().insert(new int[][]{{2, 3}, {5, 6}, {7, 9}}, new int[]{1, 8}));
-        System.out.println(new CombinationSum().combinationSum(new int[]{2, 3, 5}, 8));
+        System.out.println(new Solution().solution(new int[]{100,1000,3, 8, 4}));
     }
 }
